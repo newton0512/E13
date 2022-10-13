11f9c8394681889b4f7c56f5fe639eba78be21ce
maxnewton <newton0512@gmail.com>
2022-10-13 13:01:32 +0300

    Add Husky

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const heading = document.createElement("h1");
 heading.textContent = "Hello, world!!!";
 
 // добавляем заголовок в DOM
-const root = document.querySelector("#root")
-root.append(heading)
+const root = document.querySelector("#root");
+root.append(heading);
